686212b9966a0bdd5092b61cd2f9738f85a16af5
StarkCaptain123 <37777468+StarkCaptain123@users.noreply.github.com>
2018-10-21 17:54:06 +0530

    Add files via upload

---
+++ vcom-ui/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+})
+export class AppComponent {
+  title = "waterline";
+  closeResult: string;
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+}
---
+++ vcom-ui/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { HttpModule } from "@angular/http";
+import { LoginhomeComponent } from './loginhome/loginhome.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { PlansComponent } from './plans/plans.component';
+import { BillingComponent } from './billing/billing.component';
+
+const paths: Routes = [
+  { path: "", component: LoginhomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "billing", component: BillingComponent},
+  { path: "billing/:serviceId/:customerId", component: BillingComponent},
+  { path: "plans", component: PlansComponent }
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    LoginhomeComponent,
+    PlansComponent,
+    BillingComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    HttpModule,
+    RouterModule.forRoot(paths)
+  ],
+  providers: [],
+
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
