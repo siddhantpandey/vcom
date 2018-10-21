2d119fd16132312fe9418fe0245dd6a25c974e1d
verizon20 <verizon20@iiht.tech>
2018-10-21 14:15:34 +0530

    middleware to master

---
+++ middleware/src/main/java/com/verizon/telecom/ProjectVcomV1Application.java
@@ -0,0 +1,12 @@
+package com.verizon.telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVcomV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVcomV1Application.class, args);
+	}
+}
---
+++ middleware/src/main/java/com/verizon/telecom/services/ServicesBoughtServiceImpl.java
@@ -0,0 +1,73 @@
+package com.verizon.telecom.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.verizon.telecom.dao.ServicesBoughtDao;
+import com.verizon.telecom.model.ServicesBought;
+
+
+
+@Service
+public class ServicesBoughtServiceImpl implements ServicesBoughtService{
+	
+	@Autowired
+	ServicesBoughtDao servicesDao;
+
+	@Override
+	public ServicesBought getServicesBoughtById(long servicesboughtId) {
+		ServicesBought serviceBought = null;
+		
+		Optional<ServicesBought> optServ = servicesDao.findById(servicesboughtId);
+		if(optServ.isPresent()) {
+			serviceBought = optServ.get();
+		}
+		return serviceBought;
+		
+	}
+
+	
+
+	@Override
+	public ServicesBought addServicesBought(ServicesBought servicesbought) {
+		return servicesDao.save(servicesbought);
+	}
+
+	@Override
+	public ServicesBought updateServicesBought(ServicesBought servicesbought) {
+		return servicesDao.save(servicesbought);
+	}
+
+	@Override
+	public boolean deleteServicesBought(long servicesboughtId) {
+		boolean isDeleted = false;
+		
+		if(servicesDao.existsById(servicesboughtId)) {
+			servicesDao.deleteById(servicesboughtId);
+			isDeleted = true;
+		}
+		return isDeleted;
+	}
+
+
+
+	@Override
+	public boolean existsByServiceId(Long serviceId) {
+		return servicesDao.existsById(serviceId);
+		
+	}
+
+
+
+	@Override
+	public List<ServicesBought> getAllServices() {
+		return servicesDao.findAll();
+		
+	}
+	
+	
+
+}
